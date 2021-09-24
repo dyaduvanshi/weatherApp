3548795b92002b66e10d4d1ca1fac83d26f98e45
Deepak Singh <deepakaec6@gmail.com>
2021-09-24 16:22:30 +0530

    Working weathe widget

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Forecast } from './forecast-details';
 import { WeatherServiceService } from './weather-service.service';
-import { FormBuilder } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -9,39 +8,34 @@ import { FormBuilder } from "@angular/forms";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private ws:WeatherServiceService,public fb: FormBuilder) { }
-  cityForrecastForm = this.fb.group({
-    cityName: ''
-  })
+  constructor(private ws:WeatherServiceService) { }
+  
 
   title = 'Weather Forrecast';
 
   cities:string[]=['Toronto','Ottawa','Montreal','Calgray','Vancouver'];
-  citySelected:string='' ;
 
   cityForecast:Forecast[]=[];
   ngOnInit(): void {
     
-   this.citySelected='';
-  this.getForrecast();
+   
   }
-  onSubmit() {
-    // alert(JSON.stringify(this.oppoSuitsForm.value))
-  }
-  getForrecast(){
-    // this.citySelected=this.cityForrecastForm.get('cityName')?.value;
-    this.ws.getFutureForecast('Ottawa').subscribe((data)=>{ let temp=Object(data);
 
-      for(let i=0;i<temp.data.length;i++){
-        const tempData:Forecast={
-          day :temp.data[i].datetime,
-          temprature:temp.data[i].temp,
-          icon:temp.data[i].weather.icon
-        }
-        this.cityForecast.push(tempData);
-      }
-    }
+  
+  
+  getForrecast(city:string){
+    console.log(city);
+    this.ws.getFutureForecast(city).subscribe((data:any)=>{
       
-      );
-  }
+      let weatherDat=data.data;
+     
+      this.cityForecast=weatherDat.map((iteam:any)=>{
+       return  {
+          day:iteam.datetime,
+          temprature: iteam.temp,
+          icon:iteam.weather.icon
+
+        }
+      });
+  });}
 }
--- src/app/weather-service.service.ts
+++ src/app/weather-service.service.ts
@@ -11,7 +11,7 @@ export class WeatherServiceService {
 
   getFutureForecast(city:string){
    return  this.http.get( `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=371d4e84f04f4e6eafe38174f3de11e5&days=5`);
- 
+  // return this.http.get(`http://api.openweathermap.org/data/2.5/weather?appid=0f3fb9fa31ad3d41f1bb2bd0841c3f2f&q=${city}`);
   }
 
 
